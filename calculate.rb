81d125dee4bae7ca035e79c6f55eacf46141c935
Pavel Lobashov <ShockwaveNN@gmail.com>
2021-04-09 18:44:26 +0300

    Generate report with name of version (#8)
    * Generate report with name of version
    
    * Add one more interface
    
    * Add rake task
    
    * Add gitignore log
    
    * Fix docs
    
    * Fix documentation

--- calculate.rb
+++ calculate.rb
@@ -2,17 +2,17 @@
 
 require_relative 'lib/document_builder_recalculate_formulas'
 
-# @return [String] docbuilder executable
-DOC_BUILDER_PATH = 'documentbuilder'
 # @return [String] default path for all formulas files
 FILES_PATH = "#{ENV['HOME']}/RubymineProjects/OnlineDocuments/data/data_files/spreadsheet_formulas"
 
 files = Dir["#{FILES_PATH}/*"].sort
+recalculator = DocumentBuilderRecalculateFormulas::Recalculator.new
+puts recalculator.report_version
 
 files.each do |file|
-  script = DocumentBuilderRecalculateFormulas.form_script(file)
+  script = recalculator.form_script(file)
   puts("Processing #{file}")
   File.write('./test.docbuilder', script)
-  result = `#{DOC_BUILDER_PATH} ./test.docbuilder`
-  DocumentBuilderRecalculateFormulas.handle_result(file, result)
+  result = recalculator.builder.build_file('./test.docbuilder')
+  recalculator.handle_result(file, result)
 end
---
+++ lib/document_builder_recalculate_formulas/builder.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+# Main module of app
+module DocumentBuilderRecalculateFormulas
+  # Class for interface for builder
+  class Builder
+    # @param [String] exe path to executable
+    def initialize(exe = 'documentbuilder')
+      @exe = exe
+    end
+
+    # @return [Gem::Version, String] version of builder
+    def version
+      Gem::Version.new(cleanup_version)
+    rescue StandardError => e
+      puts("Cannot get builder version because of `#{e}`")
+      'unknown'
+    end
+
+    # Build script file
+    # @param [String] file path
+    def build_file(file)
+      `#{@exe} #{file}`
+    end
+
+    private
+
+    # @return [String] raw version string
+    def raw_version
+      `#{@exe} -v`
+    end
+
+    # @return [String] cleaned-up version string
+    def cleanup_version
+      raw_version.split("\n")
+                 .first
+                 .delete('v')
+    end
+  end
+end
