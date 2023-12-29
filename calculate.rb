e03b6eee89e927a4c05c3affd81c0447011f3fac
Vitaliy Serov <vitaliy.serov@onlyoffice.com>
2023-12-29 16:11:52 +0200

    Fix rubocop `Style/MutableConstant` cop

--- calculate.rb
+++ calculate.rb
@@ -3,7 +3,7 @@
 require_relative 'lib/document_builder_recalculate_formulas'
 
 # @return [String] default path for all formulas files
-FILES_PATH = "#{Dir.home}/RubymineProjects/OnlineDocuments/data/data_files/spreadsheet_formulas"
+FILES_PATH = "#{Dir.home}/RubymineProjects/OnlineDocuments/data/data_files/spreadsheet_formulas".freeze
 
 files = Dir["#{FILES_PATH}/*"]
 recalculator = DocumentBuilderRecalculateFormulas::Recalculator.new
