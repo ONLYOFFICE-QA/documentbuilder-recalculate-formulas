38c1e338e0e6aa850256ddfd0d54d4c25bd1b5ba
Vitaliy Serov <154601125+VitaliySerov@users.noreply.github.com>
2024-12-27 10:03:22 +0200

    Use `ruby-3.4` as project base (#371)
    * Use `ruby-3.4` as project base
    
    * Fix missing gems
    
    * Actualize todo

--- gems.rb
+++ gems.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 source 'https://rubygems.org'
+
+gem 'rake', '~> 13'
+
 group :development do
   gem 'overcommit', require: false
   gem 'rubocop', require: false
   gem 'rubocop-performance', require: false
   gem 'rubocop-rake', require: false
-  gem 'rubocop-rspec', require: false
   gem 'yard', require: false
 end
